9833bdf7964de200da366608de59b485a83eb2d8
electricStove <144375550+electricStove@users.noreply.github.com>
2024-11-08 17:02:34 -0500

    Receives input from gpredict now

--- controller-firmware/main.c
+++ controller-firmware/main.c
@@ -4,9 +4,34 @@
 #include "hardware/gpio.h"
 #include "hardware/adc.h"
 
-#define CMD_BUFFER_SIZE 100
+#define CMD_BUFFER_SIZE 20
+
+#define ADC_AZIMUTH 0
+#define ADC_ELEVATION 1
+#define ROTATE_LEFT 10
+#define ROTATE_RIGHT 11
+#define ROTATE_UP 12
+#define ROTATE_DOWN 13
+
+int get_command(char* buf);
+void parse_command(const char* buf, const int buf_len);
+void move_towards_target();
+float get_value(int pin);
+void clear_buffer(char* buf);
+void update_current_position();
+
+struct data {
+    float target_azimuth;
+    float target_elevation;
+    float current_azimuth;
+    float current_elevation;
+};
+
+struct data myData;
 
 int main() {
+    myData.current_azimuth = 180;
+    myData.current_elevation = 45;
     stdio_init_all();
     adc_init();
 
@@ -15,107 +40,138 @@ int main() {
     // Make sure GPIO is high-impedance, no pullups etc
     adc_gpio_init(27);
 
-    gpio_init(10);
-    gpio_set_dir(10, GPIO_OUT);
-    gpio_init(11);
-    gpio_set_dir(11, GPIO_OUT);
-    gpio_init(12);
-    gpio_set_dir(12, GPIO_OUT);
-    gpio_init(13);
-    gpio_set_dir(13, GPIO_OUT);
-
-    int deadzone = 100;
+    gpio_init(ROTATE_LEFT);
+    gpio_set_dir(ROTATE_LEFT, GPIO_OUT);
+    gpio_init(ROTATE_RIGHT);
+    gpio_set_dir(ROTATE_RIGHT, GPIO_OUT);
+    gpio_init(ROTATE_UP);
+    gpio_set_dir(ROTATE_UP, GPIO_OUT);
+    gpio_init(ROTATE_DOWN);
+    gpio_set_dir(ROTATE_DOWN, GPIO_OUT);
 
     char cmd_buf[CMD_BUFFER_SIZE];
     char incoming_byte;
     int cmd_len = 0;
-
-    float target_azimuth;
-    float target_elevation;
-    int stop_azimuth = 0;
-    int stop_elevation = 0;
-
     while (1) {
         // Read serial commands
-        while ((incoming_byte = getchar()) != EOF) {
-            if (incoming_byte == '\n' || incoming_byte == '\r' || incoming_byte == ' ' || incoming_byte == '\0') {
-                // EasyComm II commands are minimum 2 chars, less than that is incomplete command
-                if (cmd_len < 2)
-                    break;
-
-                cmd_buf[cmd_len] = '\0';
-                
-                // Commands are AZ, EL, ML, MR, MU, MD, SA, SE, VE
-                if (cmd_buf[0] == 'A' && cmd_buf[1] == 'Z') {
-                    stop_azimuth = 0;
-                    target_azimuth = atof(cmd_buf[2]);
-                }
-                else if (cmd_buf[0] == 'E' && cmd_buf[1] == 'L') {
-                    stop_elevation = 0;
-                    target_elevation = atof(cmd_buf[2]);
-                }
-                else if (cmd_buf[0] == 'S') {
-                    if (cmd_buf[1] == 'A') {
-                        stop_azimuth = 1;
-                    }
-                    else if (cmd_buf[1] == 'E') {
-                        stop_elevation = 1;
-                    }
-                }
-
-                cmd_len = 0;
-            }
-
-            cmd_buf[cmd_len] = incoming_byte;
-            cmd_len++;
-
-            // Command is too large
-            if (cmd_len >= CMD_BUFFER_SIZE) {
-                break;
-            }
-        }
+        cmd_len = get_command(cmd_buf);
+        parse_command(cmd_buf, cmd_len);
 
         // Rotator control loop
-        
-
-        adc_select_input(0);
-        const float conversion_factor = 3.3f / (1 << 12);
-        uint16_t result = adc_read();
-        // printf("Y-Value - Raw value: %d, voltage: %f V\n", result, result * conversion_factor);
-        if (result < 2048 - deadzone) {
-            gpio_put(10, true);
-        }
-        else
-        {
-            gpio_put(10, false);
-        }
-        if (result > 2048 + deadzone)
-        {
-            gpio_put(11, true);
-        }
-        else
-        {
-            gpio_put(11, false);
-        }
-        adc_select_input(1);
-        result = adc_read();
-        if (result < 2048 - deadzone) {
-            gpio_put(12, true);
-        }
-        else
-        {
-            gpio_put(12, false);
-        }
-        if (result > 2048 + deadzone)
-        {
-            gpio_put(13, true);
-        }
-        else
-        {
-            gpio_put(13, false);
-        }
-        // printf("X-Value - Raw value: %d, voltage: %f V\n", result, result * conversion_factor);
-        // printf("\n");
-        sleep_ms(10);
+        move_towards_target();
+        update_current_position();
+        clear_buffer(cmd_buf);
     }
-};
+}
+
+void move_towards_target() {
+    if (myData.current_azimuth < myData.target_azimuth) {
+        gpio_put(ROTATE_RIGHT, false);
+        gpio_put(ROTATE_LEFT, true);
+    }
+    else if (myData.current_azimuth > myData.target_azimuth) {
+        gpio_put(ROTATE_LEFT, false);
+        gpio_put(ROTATE_RIGHT, true);
+    }
+    else {
+        gpio_put(ROTATE_LEFT, false);
+        gpio_put(ROTATE_RIGHT, false);
+    }
+
+    if (myData.current_elevation < myData.target_elevation) {
+        gpio_put(ROTATE_DOWN, false);
+        gpio_put(ROTATE_UP, true);
+    }
+    else if (myData.current_elevation > myData.target_elevation) {
+        gpio_put(ROTATE_UP, false);
+        gpio_put(ROTATE_DOWN, true);
+    }
+    else {
+        gpio_put(ROTATE_UP, false);
+        gpio_put(ROTATE_DOWN, false);
+    }
+}
+
+int get_command(char* buf) {
+    int len = 0;
+    char incoming_byte;
+    while ((incoming_byte = getchar()) != EOF)
+    {
+        if (incoming_byte == '\n' || incoming_byte == '\r' || incoming_byte == ' ' || incoming_byte == '\0')
+        {
+            // EasyComm II commands are minimum 2 chars, less than that is incomplete command
+            if (len < 2)
+                break;
+
+            buf[len] = '\0';
+            break;
+        }
+        buf[len] = incoming_byte;
+        len++;
+
+        // Command is too large
+        if (len >= CMD_BUFFER_SIZE)
+        {
+            break;
+        }
+    }
+
+    return len;
+}
+
+void parse_command(const char* buf, const int buf_len) {
+    // Commands are AZ, EL, ML, MR, MU, MD, SA, SE, VE
+    if (buf[0] == 'A' && buf[1] == 'Z')
+    {
+        if (buf_len > 2) {
+            myData.target_azimuth = atof(buf + 2);
+        }
+        else {
+            printf("%.1f\n", myData.current_azimuth);
+            fflush(stdout);
+        }
+    }
+    else if (buf[0] == 'E' && buf[1] == 'L')
+    {
+        if (buf_len > 2) {
+            myData.target_elevation = atof(buf + 2);
+        }
+        else {
+            printf("%.1f\n", myData.current_elevation);
+            fflush(stdout);
+        }
+    }
+    else if (buf[0] == 'S')
+    {
+        if (buf[1] == 'A')
+        {
+            gpio_put(ROTATE_LEFT, false);
+            gpio_put(ROTATE_RIGHT, false);
+        }
+        else if (buf[1] == 'E')
+        {
+            gpio_put(ROTATE_UP, false);
+            gpio_put(ROTATE_DOWN, false);
+        }
+    }
+    return;
+}
+
+void update_current_position() {
+    myData.current_azimuth = get_value(ADC_AZIMUTH);
+    myData.current_elevation = get_value(ADC_ELEVATION);
+}
+
+float get_value(int pin) {
+    adc_select_input(pin);
+    // uint16_t result = adc_read();
+    // return result;
+    return adc_read();
+}
+
+void clear_buffer(char* buf) {
+    for (int i = 0; i < CMD_BUFFER_SIZE; i++)
+    {
+        buf[i] = '\0';
+    }
+}
